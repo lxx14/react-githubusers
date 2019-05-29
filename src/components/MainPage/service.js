81b2a2ee50054c3e2eca419ab99574fedb4400ed
lxx14 <lxx1427@gmail.com>
2019-05-29 10:44:58 +0300

    create base fetch request

--- src/components/MainPage/service.js
+++ src/components/MainPage/service.js
@@ -1,2 +1,6 @@
 const base_url = "https://api.github.com/";
 const all_users = "search/users?q=Dnipro";
+
+export const getUsers = (amount=10) => {
+  return fetch(`${base_url}${all_users}&per_page=${amount}`)
+}
