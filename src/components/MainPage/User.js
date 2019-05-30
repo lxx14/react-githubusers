c71f444f843f8e71035cb43c2d11d4ba831f9f20
lxx14 <lxx1427@gmail.com>
2019-05-30 11:34:06 +0300

    fix syntax

--- src/components/MainPage/User.js
+++ src/components/MainPage/User.js
@@ -10,7 +10,7 @@ const User = (props) => {
           <p>{props.name ? props.name : 'Incognito'}</p>
           <Link href={props.html_url} target='blank'>{props.login}</Link>
         </ UserContainer>
-        {props.bio ? <h3>{props.bio}</h3> : <h3>there could be his bio... But it's ampty ;)</h3>}
+        {props.bio ? <h3>{props.bio}</h3> : <h3>there could be his bio... But it's empty ;)</h3>}
         <UserContainer nameContainer mailContainer>
           {props.location ? <p> <Icon className="fas fa-map-marker-alt"></Icon> {props.location}</p> : <p><Icon className="fas fa-map-marker-alt"></Icon> From no one knows :) </p>}
           {props.email?<p><Icon className="fas fa-envelope"></Icon> {props.location} </p> : <p><Icon className="fas fa-envelope"></Icon> Mail not specified</p>}
